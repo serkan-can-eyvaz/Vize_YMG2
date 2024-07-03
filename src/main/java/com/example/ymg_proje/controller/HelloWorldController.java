2beca8d87a5e0fcba3aeafe4a28cc1b85999bd48
serkan-can-eyvaz <serkancaneyvaz@gmail.com>
2024-07-03 12:57:15 +0300

    update

--- src/main/java/com/example/ymg_proje/controller/HelloWorldController.java
+++ src/main/java/com/example/ymg_proje/controller/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
 
     @GetMapping()
     public String helloWorld(){
-        return  "  serkan can eyvazcan ";
+        return  "  serkan can eyvazcan 23 ";
 
     }
    
